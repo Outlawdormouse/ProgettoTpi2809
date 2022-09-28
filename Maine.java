9ca636aa108ca77c6d9fa9e23eeae9339f2812e0
Ricci Lorenzo <57558295+Outlawdormouse@users.noreply.github.com>
2022-09-28 09:11:10 +0200

    fine :)

--- Maine.java
+++ Maine.java
@@ -1,1 +1,14 @@
+package org.example;
+
+import static spark.Spark.*;
+import org.tinylog.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        get("/bottariIsBetterThanPollini", (req, res) -> "BottariIsBetterThanPollini");
+        get("/ciao", (req, res) -> "hola uomooooo!");
+        Logger.info("Hello World");
+    }
+}
 
